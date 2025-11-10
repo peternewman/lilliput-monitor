188b1499ddb3d3dd876bdda3f5ab86ea3c582db7
Peter Newman <peterjnewman@gmail.com>
2025-11-10 00:32:38 +0000

    Add some simple set and get encode tests too

--- lilliputd.test.js
+++ lilliputd.test.js
@@ -17,7 +17,37 @@ describe('LilliputD', () => {
 		jest.clearAllMocks()
 	})
 
-	describe('status response', () => {
+	describe('encoder', () => {
+		test('should handle setting source request', () => {
+			expect(instance.encode('source Multiviewer,SDI1-SDI2,SDI3-SDI4')).toMatchObject({
+				"encoded": Buffer.from('5a0e002001ff000000100123bcdd', 'hex'),
+				"name": "LilliputMonitor",
+			})
+		})
+
+		test('should handle setting picture request', () => {
+			expect(instance.encode('picture 10,20,30,40,50,60,6500K')).toMatchObject({
+				"encoded": Buffer.from('5a12002001ff0000010a141e28323c0261dd', 'hex'),
+				"name": "LilliputMonitor",
+			})
+		})
+
+		test('should handle setting audio request', () => {
+			expect(instance.encode('audio 50,Level,2-1')).toMatchObject({
+				"encoded": Buffer.from('5a0e002001ff000003321010dddd', 'hex'),
+				"name": "LilliputMonitor",
+			})
+		})
+
+		test('should handle getting status request', () => {
+			expect(instance.encode('status?')).toMatchObject({
+				"encoded": Buffer.from('5a0b002001ff0000fe83dd', 'hex'),
+				"name": "LilliputMonitor",
+			})
+		})
+	})
+
+	describe('decoder', () => {
 		test('should handle normal status response', () => {
 			expect(instance.decode(Buffer.from('5a470020010200fffe0033213232006402070400060000104000000400024e6f205369676e616c2020202020202020204d6f6e69746f72202020202020202020220210320110dd', 'hex'))).toMatchObject({
 				"name": "LilliputMonitor",
